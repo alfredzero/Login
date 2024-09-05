06d0b6f846f07c3cf747ee0a11fc535261605a26
alfredzero <amezcuaalfredo81@gmail.com>
2024-09-05 15:35:24 -0700

    Initial Commit

---
+++ Authentication/AuthenticationApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthenticationApp.swift
+//  Authentication
+//
+//  Created by Alfredo Amezcua on 9/5/24.
+//
+
+import SwiftUI
+
+@main
+struct AuthenticationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
