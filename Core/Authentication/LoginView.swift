14a0fcf2a4c4e6a8938013d66a7a9f2dac8215c2
alfredzero <amezcuaalfredo81@gmail.com>
2024-09-05 19:03:12 -0700

    Login Screen

--- Core/Authentication/LoginView.swift
+++ Core/Authentication/LoginView.swift
@@ -7,7 +7,11 @@
 
 import SwiftUI
 
+
 struct LoginView: View{
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
         Text("Hi World")
         NavigationStack {
@@ -20,10 +24,49 @@ struct LoginView: View{
                     .frame(width: 100, height: 100)
                     .padding(.vertical, 32)
                 // form fields
+                VStack(spacing: 24) {
+                    InputView(text: $email, title: "Email Address", placeholder: "xyz@example.com")
+                        .autocapitalization(.none)
+                        .autocorrectionDisabled()
+                    InputView(text: $password, title: "Password", placeholder: "Enter your password", isSecureField: true)
+                }
+                .padding(.horizontal)
+                .padding(.top, 12)
                 
                 //sign in button
                 
+                Button {
+                    print("Log user in")
+                } label: {
+                    HStack{
+                        Text("Sign In")
+                            .fontWeight(.semibold)
+                        Image(systemName: "arrow.right")
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: UIScreen.main.bounds.width - 32, height: 48)
+                    
+                }
+                
+                .background(Color(.systemGreen))
+                .cornerRadius(10)
+                .padding(.top, 24)
+                
+                Spacer()
+                
                 //sign up button
+                
+                NavigationLink {
+                    
+                } label: {
+                    HStack(spacing: 3){
+                        Text("Want to create an account?")
+                        Text("Sign Up")
+                            .fontWeight(.bold)
+                        
+                    }
+                    .font(.system(size:14))
+                }
             }
         }
     }
