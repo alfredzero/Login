d1a8b83bab0b2aa284a03fc034ce828f76bea3d1
alfredzero <amezcuaalfredo81@gmail.com>
2024-09-05 19:02:48 -0700

    Login Screen

---
+++ Components/InputView.swift
@@ -0,0 +1,18 @@
+//
+//  InputView.swift
+//  Authentication
+//
+//  Created by Alfredo Amezcua on 9/5/24.
+//
+
+import SwiftUI
+
+struct InputView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    InputView()
+}
---
+++ Core/Authentication/LoginView.swift
@@ -0,0 +1,34 @@
+//
+//  LoginView.swift
+//  Authentication
+//
+//  Created by Alfredo Amezcua on 9/5/24.
+//
+
+import SwiftUI
+
+struct LoginView: View{
+    var body: some View {
+        Text("Hi World")
+        NavigationStack {
+            VStack {
+                
+                // image
+                Image("")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 100, height: 100)
+                    .padding(.vertical, 32)
+                // form fields
+                
+                //sign in button
+                
+                //sign up button
+            }
+        }
+    }
+}
+
+#Preview {
+    LoginView()
+}
